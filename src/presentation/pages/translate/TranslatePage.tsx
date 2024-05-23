48010a5fc4a7aa2ae27018b5b7ed3dfd5c3773ab
edson.rosales <edson.rosales@Edsons-Macbook-Pro-Job.local>
2024-05-23 17:42:54 -0600

    feat[Translate]: Initial design with a dropdown of languages options created

--- src/presentation/pages/translate/TranslatePage.tsx
+++ src/presentation/pages/translate/TranslatePage.tsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import { GptMessage, MyMessage, TextMessageBoxSelect, TypingLoader } from "../../components";
+
+export type Message = {
+  text: string;
+  isGptMessage: boolean;
+};
+
+const languages = [
+  { id: "alemán", text: "Alemán" },
+  { id: "árabe", text: "Árabe" },
+  { id: "bengalí", text: "Bengalí" },
+  { id: "francés", text: "Francés" },
+  { id: "hindi", text: "Hindi" },
+  { id: "inglés", text: "Inglés" },
+  { id: "japonés", text: "Japonés" },
+  { id: "mandarín", text: "Mandarín" },
+  { id: "portugués", text: "Portugués" },
+  { id: "ruso", text: "Ruso" },
+];
+
 export const TranslatePage = () => {
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string, selectedOption: string) => {
+    setIsLoading(true);
+
+    const newMessage = `Traduce: "${text}" al idioma ${selectedOption}`;
+
+    setMessages( (prev) => [...prev, { text: newMessage, isGptMessage: false }] );
+
+    // TO DO: Call to the matching Use Case
+    
+    setIsLoading(false);
+
+    // TO DO: Add the isGPTMessage in true
+  };
+  
   return (
-    <div>TranslatePage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Welcome message */}
+          <GptMessage text="Hola, ¿Qué necesitas traducir?" />
+
+          {
+            messages.map( (message, index) => (
+              message.isGptMessage
+                ? (
+                  <GptMessage text={message.text} key={index} />
+                ) : (
+                  <MyMessage text={message.text} key={index} />
+                )
+            ))
+          }
+          
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
+        </div>
+      </div>
+
+      {/* Message Box */}
+      <TextMessageBoxSelect
+        onSendMessage={ handlePost }
+        placeholder="Comienza a escribir..."
+        options={languages}
+      />
+
+    </div>
   )
 };
