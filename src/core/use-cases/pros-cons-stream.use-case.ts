a2108764f0c544f8ef719f90ded7a4e6bce996de
edson.rosales <edson.rosales@Edsons-Macbook-Pro-Job.local>
2024-05-23 15:15:17 -0600

    feat[ProsConsDiscusserStream]: Initial logic to display the text as it's generated

---
+++ src/core/use-cases/pros-cons-stream.use-case.ts
@@ -0,0 +1,42 @@
+// import type { OrthographyResponse } from "../../interfaces";
+
+export const prosConsStreamUseCase = async ( prompt: string ) => {
+  try {
+    const resp = await fetch(`${import.meta.env.VITE_GPT_API}/orthography-check`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt })
+      // TO DO: abortSignal
+    });
+    
+    if ( !resp.ok ) throw new Error('No se pudo realizar la comparación');
+
+    // const data = await resp.json() as OrthographyResponse;
+    const reader = resp.body?.getReader();
+    if (!reader) {
+      console.log('No se pudo generar el reader')
+      return null;
+    }
+
+    // To decode the reader
+    const decoder = new TextDecoder();
+    let text = '';
+
+    while(true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        break;
+      }
+
+      const decodedChunk = decoder.decode(value, { stream: true });
+      text += decodedChunk;
+      console.log(text);
+    }
+
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components";
+import { prosConsStreamUseCase } from "../../../core/use-cases";
+
+export type Message = {
+  text: string;
+  isGptMessage: boolean;
+};
+
 export const ProsConsStreamPage = () => {
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text, isGptMessage: false }] );
+
+    // TO DO: Call to the matching Use Case
+    await prosConsStreamUseCase(text);
+    
+    setIsLoading(false);
+
+    // TO DO: Add the isGPTMessage in true
+  };
+  
   return (
-    <div>ProsConsStreamPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Welcome message */}
+          <GptMessage text="¿Qué cosas deseas que compare?" />
+
+          {
+            messages.map( (message, index) => (
+              message.isGptMessage
+                ? (
+                  <GptMessage text="Esto es un mensaje de GPT" key={index} />
+                ) : (
+                  <MyMessage text={message.text} key={index} />
+                )
+            ))
+          }
+          
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
+        </div>
+      </div>
+
+      {/* Message Box */}
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder="Comienza a escribir..."
+        disableCorrections
+      />
+
+    </div>
   )
 };
